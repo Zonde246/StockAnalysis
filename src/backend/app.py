087230313897681cc2c47f0bec041c4ff4f81cd2
Zonde246 <72788587+Zonde246@users.noreply.github.com>
2024-06-28 18:31:12 +0530

    Implement voting system
    Implemented Voting system for users to be able to choose which Model they want next
    
    Reformatted some HTML into components with Jinja2 Macros

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,14 +1,15 @@
+import os
+
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, jsonify, redirect
+from flask import Flask, render_template, request, jsonify, redirect, flash
 
 from supabase_client import supabase
 
 app = Flask(__name__)
 
-app.secret_key = ("console prenatal carefully thousand hydroxide nursery antarctic cataract idiocy partly graveyard "
-                  "mortify curtly decency swagger")
-
 load_dotenv()
+
+app.secret_key = os.environ.get("FLASK_APP_SECRET", "")
 
 
 @app.route('/')
@@ -32,6 +33,9 @@ def voting():
 
 @app.route('/signin')
 def signin():
+    data = supabase.auth.get_user()
+    if data is not None:
+        return redirect('/')
     return render_template('signin.html')
 
 
@@ -46,6 +50,22 @@ def changeLog():
     return render_template("changeBlogs.html")
 
 
+@app.route("/vote")
+def vote():
+    is_auth = supabase.auth.get_user()
+    if is_auth is None:
+        return redirect('/signin')
+
+    res = supabase.table("voting").select("*").execute()
+    data = []
+
+    for i in res.data:
+        # print(i)
+        data.append([i["Corporation Name"], i["Corporation Ticker value"], i["votes"]])
+
+    return render_template("vote.html", data=data)
+
+
 @app.route("/handle_auth", methods=["POST"])
 def handle_auth():
     email = request.form.get("user")
@@ -58,16 +78,44 @@ def handle_auth():
 
             # return str(data)
         except Exception as e:
-            return str(e)
+            flash(str(e))
+            return redirect('/signin')
     else:
         try:
             data = supabase.auth.sign_up(credentials={"email": email, "password": password})
             print(data)
-            return "User has been created.... Go back and sign in"
+            flash("User has been created.... please sign in")
+            return redirect('/signin')
+            # return "User has been created.... Go back and sign in"
+
         except Exception as e:
-            return str(e)
+            flash(str(e))
+            return redirect('/signin')
 
     return redirect('/')
+
+
+@app.route("/handleVote", methods=["POST"])
+def handleVote():
+    is_auth = supabase.auth.get_user()
+    if is_auth is None:
+        return redirect('/signin')
+
+    ticker = request.form.get("candidate")
+
+    res = supabase.table("userVoterRelation").select("*").eq("user", is_auth.user.id).eq("corpID", ticker).execute()
+    if len(res.data) > 0:
+        flash("You have already voted for this candidate")
+        return redirect('/vote')
+        # return "You have already voted for this candidate"
+
+    res = supabase.table("voting").select("*").eq("Corporation Ticker value", ticker).execute()
+    supabase.table("userVoterRelation").insert({"user": is_auth.user.id, "corpID": ticker}).execute()
+    supabase.table("voting").update({"votes": res.data[0]["votes"] + 1}).eq("Corporation Ticker value",
+                                                                            ticker).execute()
+
+    flash(f"Vote has been casted for {ticker}")
+    return redirect('/vote')
 
 
 if __name__ == '__main__':
